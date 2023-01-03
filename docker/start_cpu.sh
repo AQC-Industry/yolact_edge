13c2b224e631f082796359bf5c09942ccdf8efa0
wilsonveloz <wilson.veloz@onogone.com>
2023-01-03 16:14:42 +0100

    Added docker-compose launching file and Readme

--- docker/start_cpu.sh
+++ docker/start_cpu.sh
@@ -3,7 +3,7 @@
 SOURCE_CODE=$1
 DATASETS=$2
 
-docker build -t yolact_edge_cpu:latest -f Dockerfile.cpu .
+docker build --target local -t yolact_edge_cpu:latest -f Dockerfile.cpu .
 
 docker run --memory="3g" -it --name=yolact_edge \
   --shm-size=64gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
